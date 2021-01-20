b22bf2915b2f289ae6fcdaa354f2c204c34c788d
Talgat Amanbayev <amanbayev@gmail.com>
2021-01-21 05:07:57 +0600

    fixed sizing and some styles for mobile navbar. added styles and random image for login

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,18 +4,23 @@ import { NavLink } from 'react-router-dom'
 
 const MyNavbar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand as={NavLink} to="/" exact className="my-font-righteous">Booking Manager</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link as={NavLink} to="/" exact className="my-font-bebas">Home</Nav.Link>
-          <Nav.Link as={NavLink} to="/bookings" className="my-font-bebas">Bookings</Nav.Link>
-          <Nav.Link as={NavLink} to="/clients" className="my-font-bebas">Clients</Nav.Link>
-        </Nav>
-        <Nav>
-          <Nav.Link as={NavLink} to="/auth">Login</Nav.Link>
-        </Nav>
-      </Container>
+    <Navbar bg="dark" variant="dark" expand="md">
+
+      <Navbar.Brand as={NavLink} to="/" exact className="my-font-righteous">Booking Manager</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Container>
+          <Nav className="mr-auto">
+            <Nav.Link as={NavLink} to="/" exact className="my-font-bebas">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/bookings" className="my-font-bebas">Bookings</Nav.Link>
+            <Nav.Link as={NavLink} to="/clients" className="my-font-bebas">Clients</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={NavLink} to="/auth" className="my-font-bebas">Login</Nav.Link>
+          </Nav>
+        </Container>
+
+      </Navbar.Collapse>
     </Navbar>
   )
 }
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
 
-import { Button } from 'react-bootstrap'
+import { Row, Col, Button, Card, Form, InputGroup, FormControl, Image } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>
-      Login comp
-      <Button as={Link} to="/auth/register">Register</Button>
-    </div>
+    <Row className="justify-content-md-center">
+      <Col md="auto">
+        <Card style={{ width: '28rem' }}>
+          <Card.Header>
+            Login
+            </Card.Header>
+          <Card.Body>
+            <center>
+              <Image
+                style={{ width: '25rem' }}
+                src="https://www.independentcottages.co.uk/holiday-blog/wp-content/uploads/2019/04/online-booking1-1-1024x680.jpg" />
+            </center>
+            <Card.Text>
+              If you need to create a new account, please click <Link to="/auth/register">Register</Link>.
+            </Card.Text>
+
+            <Form onSubmit={(e) => { e.preventDefault(); }}>
+              <InputGroup className="mb-2 mr-sm-2">
+                <InputGroup.Prepend>
+                  <InputGroup.Text>Login</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl type="text" placeholder="Username" />
+              </InputGroup>
+              <InputGroup className="mb-2 mr-sm-2">
+                <InputGroup.Prepend>
+                  <InputGroup.Text>Password</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl type="password" placeholder="Password" />
+              </InputGroup>
+              <Button type="submit">Login</Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   )
 }
 
